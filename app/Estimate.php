d08d9e4f7c8d9fa93e5908c9b2e849fa484a1799
Jonathan Lopes <jdlopes.97@outlook.com>
2020-03-12 10:28:29 -0300

    adicionados comentários

--- app/Estimate.php
+++ app/Estimate.php
@@ -4,10 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Classe modelo de orçamentos
+ * 
+ * @author Jonathan David Mendes Lopes
+ * @version 1.1
+ */
 class Estimate extends Model
 {
     protected $guarded = [];
-
+    /**
+     * Define a relação muitos-para-um de orçamento para usuário(vendedor) 
+     */
     public function user(){
         return $this->belongsTo(User::class);
     }
--- app/Http/Controllers/EstimatesController.php
+++ app/Http/Controllers/EstimatesController.php
@@ -9,15 +9,35 @@ use App\Http\Requests\StoreFormValidation;
 use SebastianBergmann\Environment\Console;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Controlador da classe de orçamentos com operções CRUD
+ * 
+ * @author Jonathan David Mendes Lopes
+ * @version 1.1
+ */
+
 class EstimatesController extends Controller
 {
-
+    /**
+     * Povoa a lista de orçamentos
+     * 
+     * @return view Retorna a tela index com a lista de orcamentos 
+     */
     public function index()
     {
         $estimates = Estimate::whereNotNull('id')->latest()->paginate(5);
         return view('estimates.index', compact('estimates'));
     }
-
+    /**
+     * Pesquisa orçamentos
+     * <p>
+     * Primeiramente todos os dados são puxados do BD 
+     * depois vão sendo filtrados de acordo com os parametros fornecidos
+     * 
+     * @param $request requisição que veio do formulário de busca
+     * 
+     * @return view retorna a tela de lista de orçamentos com os dados filtrados
+     */
     public function search(Request $request)
     {
         $sellerName = $request->get('seller');
@@ -55,7 +75,14 @@ class EstimatesController extends Controller
         }
         return view('estimates.index')->withMessage("Nenhum resultado encontrado");
     }
-
+    /**
+     * @method GET
+     * Edita um orçamento já existente
+     * <p>
+     * Somente usuários logados podem realizar esta operação
+     * 
+     * @return view Retorna a tela de edição do orçamento selecionado
+     */
     public function edit(Estimate $estimate)
     {
         if (!Auth::check()) {
@@ -64,6 +91,17 @@ class EstimatesController extends Controller
         return view('estimates.update', compact('estimate'));
     }
 
+    /**
+     * @method POST
+     * Atualiza os dados de um orçamento no BD
+     * <p>
+     * Somente usuários logados podem realizar esta operação,
+     * 
+     * @param estimate O orçamento que está sendo atualizado
+     * @param request A requisição é validada na classe StoreFormValidation
+     * 
+     * @return redirect Redireciona para a tela com a lista de orçamentos
+     */
     public function update(Estimate $estimate, StoreFormValidation $request)
     {
         if (!Auth::check()) {
@@ -73,11 +111,27 @@ class EstimatesController extends Controller
         return redirect("/orcamentos");
     }
 
+    /**
+     * @method GET
+     * Cria um novo orçamentos
+     * 
+     * @return view retorna a tela de cadastro de um novo orçamento
+     */
     public function create()
     {
         return view('estimates.create');
     }
-
+    /**
+     * @method POST
+     * Cria um novo orçamento
+     * <p>
+     * O campo $instant do orçamento é preenchido com a data atual, 
+     * então o novo orçamento é armazenado no BD
+     * 
+     * @param request a requisição é validada na classe StoreFormValidation
+     * 
+     * @return redirect Redireciona para a tela com a lista de orçamentos
+     */
     public function store(StoreFormValidation $request)
     {
         $data = $request->all();
@@ -88,11 +142,24 @@ class EstimatesController extends Controller
         return redirect('/orcamentos');
     }
 
+    /**
+     * Mostra os detalhes completos do orçamento
+     * 
+     * @param $estimate O orçamento que está sendo atualizado  
+     * 
+     * @return view Retorna a tela show com os detalhes deste orçamento
+     */
     public function show(Estimate $estimate)
     {
         return view('estimates.show', compact('estimate'));
     }
-
+    /**
+     * Deleta um orçamento do BD
+     * 
+     * @param $estimate O orçamento que será deletado  
+     * 
+     * @return redirect Redireciona para a tela com a lista de orçamentos
+     */
     public function destroy(Estimate $estimate)
     {
         if (!Auth::check()) {
